e526d954010cacd3874163aacc15d61dd1cb1dc8
Oluwatosin Gbenga <tosinolugbenga@gmail.com>
2024-06-08 03:37:37 +0100

    initial setup

---
+++ src/example-client.js
@@ -0,0 +1,35 @@
+const BookSearchApiClient = require("./BookSearchApiClient.js");
+
+const apiConfig = {
+  sellers: [
+    {
+      name: "BookSeller1",
+      baseUrl: "http://api.book-seller1-example.com",
+      format: "json",
+    },
+    {
+      name: "BookSeller2",
+      baseUrl: "http://api.book-seller2-example.com",
+      format: "xml",
+    },
+  ],
+};
+
+const client = new BookSearchApiClient(apiConfig);
+
+(async () => {
+  try {
+    console.log(`Fetching books from all configured sellers`);
+
+    const booksByShakespeare = await client.getBooksByAuthor("Shakespeare", 10);
+    console.log("Books by Shakespeare:", booksByShakespeare);
+
+    const booksByPublisher = await client.getBooksByPublisher("Penguin", 10);
+    console.log("Books by Publisher:", booksByPublisher);
+
+    const booksByYear = await client.getBooksByYear(2023, 10);
+    console.log("Books by Year:", booksByYear);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+})();
